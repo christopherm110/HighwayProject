8c6e77cab41a3d8257998229efd097a19b349634
christopherm110 <167322395+christopherm110@users.noreply.github.com>
2024-05-07 09:30:03 -0400

    Added points and increased speed every 10 seconds
    Added a new point system in which 10 points are awarded every time you successfully pass a car and it goes off-screen. For next time, add a point deduction for crashing, also a high score system that could be saved as well.
    
    Began adding a feature in which the cars will gradually become faster every 10 seconds, but needs more tweaking to make the increase noticeable.
    
    Noticed a bug in which after a few seconds (unsure how long) of playing, the ghost mode will run permanently even after 3 seconds, and needs to be fixed.

--- main1.py
+++ main1.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-import random
+# import random
 from car import Car
 from traffic import Traffic
 from traffic2 import Traffic2
@@ -24,9 +24,9 @@ screen = pygame.display.set_mode(size)
 message = "Points: 0"
 
 c = Car(535, 500)
-t = Traffic(430, (random.randint(250, 500)))
-t2 = Traffic2(620, -100)
-t3 = Traffic3(525, random.randint(0, 250))
+t = Traffic(430, -720)
+t2 = Traffic2(620, -360)
+t3 = Traffic3(525, 0)
 bg = Background(280, 0)
 ex = Explosion(-1000, 0)
 
@@ -41,20 +41,44 @@ time_remaining = 3
 ghost_start_time = 0
 ghost_time = 0
 collided_car = "none"
+other_cars = [t, t2, t3]
 start_time = time.time()
+milestone = 10
+milestone_reached = False
+milestone_message = round((milestone - 10) / 10)
 
-display_message = my_font.render(message, True, (255, 255, 255))
+display_points = my_font.render(message, True, (255, 255, 255))
 display_time = my_font.render(time_elapsed, True, (255, 255, 255))
+display_milestone = my_font.render("Milestone: " + str(milestone_message), True, (255, 255, 255))
 
 run = True
 
 while run:
+
+    # Time
     current_time = time.time()
-
     time_elapsed = round(current_time - start_time, 1)
+    display_time = my_font.render(str(time_elapsed) + "s", True, (255, 255, 255))
 
+    if time_elapsed == milestone:
+        milestone_reached = True
+
+    if milestone_reached:
+        milestone_reached = False
+        milestone = milestone + 10
+        for cars in other_cars:
+            cars.delta = cars.delta + 0.1
+    display_milestone = my_font.render("Milestone: " + str(milestone_message), True, (255, 255, 255))
+
+    # Point System
+    for car in other_cars:
+        if car.detect_off_screen():
+            points = points + 10
+            display_points = my_font.render("Points: " + str(points), True, (255, 255, 255))
+
+    # Key Inputs
     keys = pygame.key.get_pressed()
-
+    # Vertical Movement
     if keys[pygame.K_w] and not exploded:
         c.y_vel = c.y_vel + 2
         c.move_direction("up")
@@ -68,35 +92,31 @@ while run:
         t2.traffic_movement()
         t3.traffic_movement()
 
+    # Ghost Mode
     if collisions == 1 and not ghost_used:
         ghost = True
         ghost_used = True
-
     if collisions == 2:
         exploded = True
-
     if ghost:
         ghost_start_time = time.time()
         pygame.Surface.set_alpha(c.image, 125)
         ghost_time = round(ghost_start_time - start_time, 1)
-        if ghost_time == 3:
-            ghost = False
-
+    if ghost_time == 3:
+        ghost = False
     if not ghost:
         pygame.Surface.set_alpha(c.image, 255)
         # Collisions
-        other_cars = [t, t2, t3]
         for car in other_cars:
             if pygame.Rect.colliderect(c.rect, car.rect):
                 collided_car = car
                 collisions = collisions + 1
 
-    display_time = my_font.render(str(time_elapsed) + "s", True, (255, 255, 255))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+        # Horizontal Movement
         if event.type == pygame.KEYDOWN and not exploded:
             if event.key == 97:
                 c.move_direction("left")
@@ -104,12 +124,11 @@ while run:
                 c.move_direction("right")
 
     # bg.move_direction("up")
-    # fog.set_alpha(127)
-    # screen.blit(fog, (0, 0))
     screen.fill((106, 190, 48))
     screen.blit(bg.image, bg.rect)
-    screen.blit(display_message, (1, 0))
+    screen.blit(display_points, (1, 0))
     screen.blit(display_time, (1, 20))
+    screen.blit(display_milestone, (1, 40))
     screen.blit(t.image, t.rect)
     screen.blit(t2.image, t2.rect)
     screen.blit(t3.image, t3.rect)
--- traffic3.py
+++ traffic3.py
@@ -13,7 +13,9 @@ class Traffic3:
         # self.image = pygame.transform.scale(self.image, scale_size)
         # self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-        self.delta = 1
+        self.delta = 2
+        self.points = 0
+        self.points_given = False
 
     def move_direction(self, direction):
         if direction == "up":
@@ -25,9 +27,18 @@ class Traffic3:
     def traffic_movement(self):
         self.move_direction("down")
         if self.y > 1208:
+            self.points = self.points + 10
             self.y = random.randint(-500, -128)
             random_speed = random.randint(-5, 5)
             speed = random_speed / 10
             self.delta = self.delta + speed
             if self.delta < 1:
                 self.delta = 1
+
+    def detect_off_screen(self):
+        if self.y > 720 and not self.points_given:
+            self.points_given = True
+            return True
+        if 0 < self.y < 720 and self.points_given:
+            self.points_given = False
+            return False
